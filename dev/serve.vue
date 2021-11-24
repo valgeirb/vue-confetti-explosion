eca6982fb8c7be3266d800fa4e0a84ff55c609ab
Valgeir Björnsson <valgeirb@nextgl.com>
2021-11-24 16:33:40 +0000

    📦 NEW: Start porting to Vue

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import ConfettiExplosion from '@/confetti-explosion.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    ConfettiExplosion
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <confetti-explosion />
+  </div>
+</template>
